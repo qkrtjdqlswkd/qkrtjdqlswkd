e548c6e619e065219abead4729adb557271fd625
Mr.Park <150650265+qkrtjdqlswkd@users.noreply.github.com>
2025-04-21 15:14:06 -0400

    Update readme.js

--- readme.js
+++ readme.js
@@ -4,9 +4,8 @@ const abtMe = {
         last: "Park",
     },
     experiences: {
-        curr: 'Building an AI 3d-avatar conversion program that can attach new clothes on ',
-        Prev: 'Built ML model that predicts future city-line requests based on locations & timestamps',
-        Prev2: 'Built ML movie data revenue model, loaded into SQL. Then, using Gemini API, built custom SQL query function that allow for natural interation with the movie data'
+        curr: 'Building an AI 3D-avatar conversion program that can attach new clothes on ',
+        Most_Prev: 'Built ML model that predicts future city-line requests based on locations & timestamps',
     },
     cur_learning: "Learning pytorch-based PIFUHD to convert 2D to 3d.",
 

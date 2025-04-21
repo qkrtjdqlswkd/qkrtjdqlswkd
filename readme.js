d124d3d1965fce6ae919d5b93ce51457d46209c4
Mr.Park <150650265+qkrtjdqlswkd@users.noreply.github.com>
2025-04-21 15:10:16 -0400

    Add files via upload

---
+++ readme.js
@@ -0,0 +1,17 @@
+const abtMe = {
+    name: {
+        First: "Sungbin",
+        last: "Park",
+    },
+    experiences: {
+        curr: 'Building an AI 3d-avatar conversion program that can attach new clothes on ',
+        Prev: 'Built ML model that predicts future city-line requests based on locations & timestamps',
+        Prev2: 'Built ML movie data revenue model, loaded into SQL. Then, using Gemini API, built custom SQL query function that allow for natural interation with the movie data'
+    },
+    cur_learning: "Learning pytorch-based PIFUHD to convert 2D to 3d.",
+
+    Socials:{
+        linkedin: "https://www.linkedin.com/in/sungbin-park-59b436327/",
+        email: "bseongbin402@gmail.com",
+    }
+}
